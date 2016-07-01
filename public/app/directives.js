053a243adf6c2b62d5d8509fc46573702a94956c
Rafael Meza <rmeza@rcp.pe>
2016-07-01 11:28:19 -0500

    Add menu

--- public/app/directives.js
+++ public/app/directives.js
@@ -3,5 +3,25 @@
 
   angular
     .module('main-app')
+     .directive("onClickShowMenu", onClickShowMenu)
+
+    onClickShowMenu.$inject = ['$state'];
+    function onClickShowMenu($state){
+      return {
+        restrict: "AE",
+        scope: {},
+        link: function(scope, el, attr){
+        	var html = angular.element('html');
+        	el.on('click', function(e){
+        		var self = angular.element(this);
+        		if(!html.hasClass('isMenuOpened')){
+        			html.addClass('isMenuOpened');
+        		}else{
+        			html.removeClass('isMenuOpened');
+        		}
+        	});
+        }
+      }
+    }
 
 })();
