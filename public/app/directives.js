dac9d9ead3b9b9f4b752f6f901b332d9ec5f9b20
raffo1234 <ralphch1234562@hotmail.com>
2016-07-02 11:50:23 -0500

    Add portfolio page

--- public/app/directives.js
+++ public/app/directives.js
@@ -13,22 +13,26 @@
         link: function(scope, el, attr){
         	var html = angular.element('html');
         	el.on('click', function(e){
-            
-            if(typeof attr.openClose !== 'undefined'){
-          		if(!html.hasClass('isMenuOpened')){
-          			html.addClass('isMenuOpened');
-          		}else{
-          			html.removeClass('isMenuOpened');
-          		}
-            }
             if(attr.uiHref){
               angular.element('html').addClass('isStateChanging');
               $timeout(function(){
                 $state.go(attr.uiHref);
                 $timeout(function(){
                   angular.element('html').removeClass('isStateChanging');
-                }, 360);
-              }, 360);
+                }, 300);
+              }, 300);
+            }
+            if(typeof attr.close !== 'undefined'){
+              html.removeClass('isMenuOpened');
+            }
+            if(typeof attr.openClose !== 'undefined'){
+              if(!html.hasClass('isMenuOpened')){
+                html.addClass('isMenuOpened');
+              }else{
+                $timeout(function(){
+                  html.removeClass('isMenuOpened');
+                }, 300);
+              }
             }
         	});
         }
